688484e24255ea3eeb12a0a624416304d86dfd6d
SAMUEL CARVALHO DE ALMEIDA <samuelca.senai@fieg.com.br>
2025-10-10 11:38:28 -0300

    teste imagens no drive

--- main.py
+++ main.py
@@ -113,11 +113,11 @@ def baixar_imagem_por_nome(nome_imagem, pasta_id):
 # Baixar imagem específica pelo nome
 imagem_x = baixar_imagem_por_nome("X.jpg", st.secrets["id_imagens"])
 if imagem_x:
-    st.image(imagem_x, caption="X.jpg", use_column_width=True)
+    st.image(imagem_x, caption="X.jpg", use_container_width=True)
 
 imagem_cafe = baixar_imagem_por_nome("café.jpg", st.secrets["id_imagens"])
 if imagem_cafe:
-    st.image(imagem_cafe, caption="café.jpg", use_column_width=True)
+    st.image(imagem_cafe, caption="café.jpg", use_container_width=True)
 
 client = gspread.authorize(creds) #Acessando sheets
 
